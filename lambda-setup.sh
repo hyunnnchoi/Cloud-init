95d83e5eb8e5500f930577860ee693f8521e5157
Hyunmok Choi <37583279+hyunnnchoi@users.noreply.github.com>
2025-04-02 13:09:40 +0900

    Update lambda-setup.sh

--- lambda-setup.sh
+++ lambda-setup.sh
@@ -191,7 +191,7 @@ install_kubernetes() {
     git clone https://github.com/hyunnnchoi/Cloud-init.git
 
     cd Cloud-init
-
+    git checkout k8s
     chmod -R 777 /home/tensorspot
 
     cat apt_archives_part_* | tee merged.tar.gz > /dev/null
